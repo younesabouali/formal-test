938a25b8ba06602650225350a9a3359d3a43bd5e
Younes Abouali <younesabouali3@gmail.com>
2023-06-13 17:35:46 +0100

    implementing the navigation link

--- app/AppHeader.tsx
+++ app/AppHeader.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import React from "react";
+import Link from 'next/link'
+
 import { Divider } from "./shared/components/Divider";
 interface AppHeaderProps{
   border?:boolean
@@ -9,14 +11,29 @@ export const AppHeader = (props: AppHeaderProps) => {
   return (
     <div className="flex flex-row justify-between">
       <Image src={"/logo.svg"} alt="Logo"width={"48"} height={34.17}/>
+      <div className="w-full h-[inherit] flex flex-col justify-between px-7">
       {
         border ? 
-      <div className="w-full h-[inherit] flex flex-col justify-between px-7">
         <Divider/>
-        <Divider/>
-      </div>:null
+ 
+        :null
+  
+        }
+        <div className="flex justify-between w-full">
 
-      }
+        <Link href="/" className="text-thrive-dark-blue underline">Home screen</Link>
+        <Link href="/industries" className="text-thrive-dark-blue underline">Industries</Link>
+        <Link href="/investement-stats"  className="text-thrive-dark-blue underline">Investement Stats</Link>
+        </div>
+        {
+        border ? 
+        <Divider/>
+ 
+        :null
+  
+        }
+        
+      </div>
       <Image src={"/avatar.png"} alt="Avatar"width={"48"} height={"48"}/>
     </div>
   );
